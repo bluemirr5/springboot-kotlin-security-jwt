e5daf6a70fde71a48b7021431250a04778d05906
gswon <bluemirr5@gmail.com>
2018-06-07 14:02:31 +0900

    init

---
+++ src/main/kotlin/kr/rang2ne/examples/springbootkotlinsecurityjwt/SpringbootKotlinSecurityJwtApplication.kt
@@ -0,0 +1,11 @@
+package kr.rang2ne.examples.springbootkotlinsecurityjwt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootKotlinSecurityJwtApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootKotlinSecurityJwtApplication>(*args)
+}
---
+++ src/test/kotlin/kr/rang2ne/examples/springbootkotlinsecurityjwt/SpringbootKotlinSecurityJwtApplicationTests.kt
@@ -0,0 +1,16 @@
+package kr.rang2ne.examples.springbootkotlinsecurityjwt
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpringbootKotlinSecurityJwtApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
